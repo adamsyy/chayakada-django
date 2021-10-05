f93a06a08024a5050b33cd3e2a5df29445ecd427
Asher Mathews <msasher123@gmail.com>
2021-10-05 14:09:14 +0530

    MAde some changes to requirements.txt

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django',
         'NAME': 'chayakada',
     }
 }
